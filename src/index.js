b208b83e822dfc74da98c4c773660fb9928d69ae
xenock <miguelmoracho@gmail.com>
2018-09-22 11:25:05 +0200

    added cards and testing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 
 /* global PIXI */
 
-import patrick from './patrick.js';
+import patrick from './patrick.js'
+import {getDeck} from './fakeFetch.js'
+import card from '../assets/cards/symbol10.png'
 
-const app = new PIXI.Application(window.innerWidth, window.innerHeight, {backgroundColor : 0x1099bb});
-document.body.appendChild(app.view);
-app.stage.addChild(patrick);
+const app = new PIXI.Application(window.innerWidth, window.innerHeight, {backgroundColor : 0x1099bb})
+document.body.appendChild(app.view)
+app.stage.addChild(patrick)
+app.stage.addChild(card)
 
-app.ticker.add(function(delta) {
-  patrick.rotation += 0.01 * delta;
-});
+console.log('this is a ', getDeck('../assets',8))
+
+app.ticker.add(delta => {
+  patrick.rotation += 0.01 * delta
+})
